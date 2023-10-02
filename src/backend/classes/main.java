d10e7e92346990f85eac0fdec82845bfd0f504b4
Justinchunng@gmail.com <Justinchunng@gmail.com>
2023-10-02 14:33:27 -0400

    Modified main.java test file

--- src/backend/classes/main.java
+++ src/backend/classes/main.java
@@ -4,12 +4,14 @@ import java.util.ArrayList;
 
 public class main {
     public static void main(String args[]){
-        ClickButton cb = new ClickButton();
-        cb.execute(null);
         ArrayList<TestAction> l = new ArrayList<TestAction>();
-        l.add(cb);
+        l.add(new ClickButton());
+        l.add(new EnterText("testing"));
+        l.add(new SelectInputBox());
+        l.add(new TakeScreenshot());
+        l.add(new VisitUrl("www.hobs.com"));
         for (TestAction testAction : l) {
-            testAction.execute(null);
+            testAction.execute();
         }
     }
 }
