cfcf42553736196f87cf5f0acdf8d06af1eaf477
Matteo <matteomollano@gmail.com>
2023-10-18 13:34:22 -0400

    updated chromedriver path

--- src/backend/classes/App.java
+++ src/backend/classes/App.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class App {
     public static void main(String args[]) {
-        System.setProperty("webdriver.chrome.driver", "/Users/matteomollano/Downloads/chromedriver-mac-x64/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "chromedriver-mac-x64/chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.amazon.com/");
     }
