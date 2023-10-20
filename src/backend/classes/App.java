ffead32737ddb443e97e8d8d7a7b9cb599204786
Matteo <matteomollano@gmail.com>
2023-10-20 15:48:48 -0400

    updated App.java

--- src/backend/classes/App.java
+++ src/backend/classes/App.java
@@ -2,11 +2,15 @@ package backend.classes;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class App {
     public static void main(String args[]) {
         System.setProperty("webdriver.chrome.driver", "chromedriver-linux64/chromedriver");
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setBinary("chromedriver-linux64/headless-chromium");
+        chromeOptions.addArguments("--headless");
+        WebDriver driver = new ChromeDriver(chromeOptions);
         driver.get("https://www.amazon.com/");
     }
 }
