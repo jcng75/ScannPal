8834f99235a4a6a86910eae834728b81b6830636
jcng75 <justinchunng@gmail.com>
2024-01-28 19:20:34 -0500

    Created HeuristicsCheck java class, updated TestCase.java file

--- src/backend/classes/TestCase.java
+++ src/backend/classes/TestCase.java
@@ -1,6 +1,8 @@
 package backend.classes;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+
 import java.io.Serializable;
 
 public class TestCase implements Serializable {
@@ -10,15 +12,24 @@ public class TestCase implements Serializable {
         this.testCases = new ArrayList<TestAction>();
     }
 
+    public TestCase(List<TestAction> actions) {
+        this.testCases = actions;
+    }
+
     public List<TestAction> getTestCase() {
         return this.testCases;
     }
 
-    public void append(TestAction action) {
-        this.testCases.add(action);
+    public void append(TestAction action, HashSet<String> hashSet) {
+        HeuristicsCheck hc = new HeuristicsCheck();
+        // Modify this later on
+        if (!hc.heuristicsCheck(null, null, hashSet)){
+            this.testCases.add(action);
+        }
     }
 
     public void display() {
         System.out.println(testCases);
     }
+
 }
