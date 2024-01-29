350f72dc2c707147276f375228bd51083d51efb1
jcng75 <justinchunng@gmail.com>
2024-01-28 19:58:06 -0500

    Modified WebCrawler to follow fu algorithm

--- src/backend/classes/webCrawler.java
+++ src/backend/classes/webCrawler.java
@@ -1,6 +1,8 @@
 package backend.classes;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
@@ -57,89 +59,33 @@ public class WebCrawler {
         loginButton.execute();
     }
 
-    
-    private String parseURLHost(String url){
-        try {
-            URL fullUrl = new URL(url);
-            String startUrl = fullUrl.getHost();
-            return startUrl;
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        return url;
-        
-    }
-    
-    private String parseURLPath(String url){
-        try {
-            URL fullUrl = new URL(url);
-            String path = fullUrl.getPath();
-            return path;
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        return url;
-    }
-
-    private boolean isStale(WebElement element){
-        return ExpectedConditions.stalenessOf(element).apply(MyWebDriver.getDriver());
-    }
-
-    private boolean isDifferentWebsite(WebElement element) {
-        String link = element.getAttribute("href");
-        return !this.parsedUrl.equals(parseURLHost(link));
-    }
-
-    private boolean isSameLink(WebElement element, String currentLink){
-        String link = element.getAttribute("href");
-        return link == currentLink; 
-    }
-
-    private boolean isLogout(WebElement element){
-
-        LinkedList<String> bannedList = new LinkedList<String>(Arrays.asList("logout", "signout", "log out", "sign out"));
-        String elementText = element.getText().toLowerCase();
-        return bannedList.contains(elementText);
-        
-    }
-
-    private boolean heuristicsCheck(WebElement element, String currentLink){
-        
-        // if any of the heuristics hold, we can skip the web element within the crawl function
-        if (isStale(element)) return true;
-        if (isDifferentWebsite(element)) return true;
-        if (isSameLink(element, currentLink)) return true;
-        if (isLogout(element)) return true;
-
-        return false;
-    }
-
-
+    /*
     public boolean crawl(int depth){
-
+        
         System.out.println(parseURLPath(url));
         String s = String.format("Crawling for website: %s", url);
         System.out.println(s);
-
+        
         WebDriver driver = MyWebDriver.getDriver();
         VisitUrl visitUrl = new VisitUrl(this.url);
         visitUrl.execute();
-
+        
         loginUser();
         System.out.println(String.format("Logged into website: %s", driver.getTitle()));
         System.out.println(String.format("Current URL: %s", driver.getCurrentUrl()));
-
+        
         String currentURL = driver.getCurrentUrl();
-
+        
         // Initialize web element list that will contain all possible action paths
         LinkedList<LinkedList<String>> elementList = new LinkedList<LinkedList<String>>();
         // Initalize link elements queue that will help us navigate through webpage
         // Add the starting link to the queue
         Queue<LinkedList<String>> linkQueue = new LinkedList<LinkedList<String>>();
         LinkedList<String> initialLink = new LinkedList<String>(Arrays.asList(currentURL));
+        // add a hashset
         linkQueue.add(initialLink);
         elementList.add(initialLink);
-
+        
         // Need a depth counter to make sure we go the specified depth of the user
         int depthCounter = 0;
         // The currLinkCounter is used to track the total links from each "layer"
@@ -174,89 +120,59 @@ public class WebCrawler {
             }
             
         }
-
+        
         System.out.println(elementList);
-
+        
         // run crawler and then store information
         return true;
     }
-
-    public boolean crawl2(int depth){
-
-        System.out.println(parseURLPath(url));
+    */
+    
+    // Fu version
+    public List<TestCase> crawl2(int depth){
+        
         String s = String.format("Crawling for website: %s", url);
         System.out.println(s);
-
+        
         WebDriver driver = MyWebDriver.getDriver();
         VisitUrl visitUrl = new VisitUrl(this.url);
         visitUrl.execute();
-
+        
         loginUser();
         System.out.println(String.format("Logged into website: %s", driver.getTitle()));
         System.out.println(String.format("Current URL: %s", driver.getCurrentUrl()));
-
+        
         String currentURL = driver.getCurrentUrl();
+        
+        // Add the starting link to the first queue
+        Queue<TestCase> currentQueue = new LinkedList<TestCase>();
+        Queue<TestCase> nextQueue = new LinkedList<TestCase>();
 
-        // Initialize web element list that will contain all possible action paths
-        LinkedList<LinkedList<TestAction>> elementList = new LinkedList<LinkedList<TestAction>>();
-        // Initalize link elements queue that will help us navigate through webpage
-        // Add the starting link to the queue
-        Queue<LinkedList<String>> linkQueue = new LinkedList<LinkedList<String>>();
+        // Hashset to make unique links only
+        HashSet<String> hashSet = new HashSet<String>();
+        
+        // Initialize starting test case
         // Create the login action
         EnterText enterUser = new EnterText("username", this.username);
         EnterText enterPassword = new EnterText("password", this.password);
         ClickButton loginButton = new ClickButton("Login");
-        LinkedList<TestAction> initialLogin = new LinkedList<TestAction>(Arrays.asList(enterUser, enterPassword, loginButton));
-        LinkedList<String> initialLink = new LinkedList<String>(Arrays.asList(currentURL));
-        elementList.add(initialLogin);
-        linkQueue.add(initialLink);
-        /* TODO 
-         * Remade the elementList to store TestActions, now implement the testactions instead of strings into the list
-         * Current questions:
-         * - Very easy to make the websites visit the next link on the page using VisitUrl.execute(), but how can we do this for clicking buttons?
-         * - If we enter text into a web element (EX: input) how do we find the button associated with it to click?
-         * -> idea: use releative locations
-         * - How should I store the queue?  In the original test, I used strings to go to the next element, but should i store the queue as testactions instead? 
-         */
-        // Need a depth counter to make sure we go the specified depth of the user
-        int depthCounter = 0;
-        // The currLinkCounter is used to track the total links from each "layer"
-        // The newLinkCounter tracks it for the next layer
-        int currLinkCounter = 1;
-        int newLinkCounter = 0;
-        while (depthCounter != depth && !linkQueue.isEmpty()){
-            System.out.println("Current depth: " + depthCounter);
-            // System.out.println("Current element queue: " + linkQueue);
-            // Extract the first element of queue and the link associated with the element
-            LinkedList<String> currentList = linkQueue.poll();
-            String currentLink = currentList.getLast();
-            // System.out.println(currentLink);
-            // Go to new link
-            List<WebElement> elements = MyWebDriver.getDriver().findElements(By.tagName("a"));
-            // System.out.println("Current queue: " + linkQueue);
-            for (WebElement element : elements){
-                String newLink = element.getAttribute("href");
-                if (heuristicsCheck(element, currentLink)) continue;
-                LinkedList<String> newList = new LinkedList<String>(currentList);
-                newList.add(newLink);
-                linkQueue.add(newList);
-                // elementList.add(newList);
-                newLinkCounter++;
+        List<TestAction> initialLogin = new ArrayList<TestAction>(Arrays.asList(enterUser, enterPassword, loginButton));
+        TestCase initialTestCase = new TestCase(initialLogin);
+        
+        nextQueue.add(initialTestCase);
+        
+        for (int i = 0; i < depth ; i++){
+            currentQueue = nextQueue;
+            nextQueue = null;
+            for (TestCase tc : currentQueue){
+                //List<TestCase> updatedTC = tc.append(tc, hashSet); 
+                //nextQueue.addAll(updatedTC);
             }
-            currLinkCounter--;
-            // If the current link counter reaches 0 we must go to the next depth of scanning
-            if (currLinkCounter == 0){
-                depthCounter++;
-                currLinkCounter = newLinkCounter;
-                newLinkCounter = 0;
-            }
-            
         }
 
-        System.out.println(elementList);
-
-        // run crawler and then store information
-        return true;
+        // Return information
+        List<TestCase> resultList = (List) currentQueue;
+        return resultList;
     }
 
 }
