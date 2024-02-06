ea118f1462257771574b5053ddd7e204a23bef63
jcng75 <justinchunng@gmail.com>
2024-02-06 12:09:54 -0500

    Renamed and cleaned crawl code

--- src/backend/classes/webCrawler.java
+++ src/backend/classes/webCrawler.java
@@ -52,78 +52,9 @@ public class WebCrawler {
         ClickButton loginButton = new ClickButton("Login");
         loginButton.execute();
     }
-
-    /*
-    public boolean crawl(int depth){
-        
-        System.out.println(parseURLPath(url));
-        String s = String.format("Crawling for website: %s", url);
-        System.out.println(s);
-        
-        WebDriver driver = MyWebDriver.getDriver();
-        VisitUrl visitUrl = new VisitUrl(this.url);
-        visitUrl.execute();
-        
-        loginUser();
-        System.out.println(String.format("Logged into website: %s", driver.getTitle()));
-        System.out.println(String.format("Current URL: %s", driver.getCurrentUrl()));
-        
-        String currentURL = driver.getCurrentUrl();
-        
-        // Initialize web element list that will contain all possible action paths
-        LinkedList<LinkedList<String>> elementList = new LinkedList<LinkedList<String>>();
-        // Initalize link elements queue that will help us navigate through webpage
-        // Add the starting link to the queue
-        Queue<LinkedList<String>> linkQueue = new LinkedList<LinkedList<String>>();
-        LinkedList<String> initialLink = new LinkedList<String>(Arrays.asList(currentURL));
-        // add a hashset
-        linkQueue.add(initialLink);
-        elementList.add(initialLink);
-        
-        // Need a depth counter to make sure we go the specified depth of the user
-        int depthCounter = 0;
-        // The currLinkCounter is used to track the total links from each "layer"
-        // The newLinkCounter tracks it for the next layer
-        int currLinkCounter = 1;
-        int newLinkCounter = 0;
-        while (depthCounter != depth && !linkQueue.isEmpty()){
-            System.out.println("Current depth: " + depthCounter);
-            // System.out.println("Current element queue: " + linkQueue);
-            // Extract the first element of queue and the link associated with the element
-            LinkedList<String> currentList = linkQueue.poll();
-            String currentLink = currentList.getLast();
-            // System.out.println(currentLink);
-            // Go to new link
-            List<WebElement> elements = MyWebDriver.getDriver().findElements(By.tagName("a"));
-            // System.out.println("Current queue: " + linkQueue);
-            for (WebElement element : elements){
-                String newLink = element.getAttribute("href");
-                if (heuristicsCheck(element, currentLink)) continue;
-                LinkedList<String> newList = new LinkedList<String>(currentList);
-                newList.add(newLink);
-                linkQueue.add(newList);
-                elementList.add(newList);
-                newLinkCounter++;
-            }
-            currLinkCounter--;
-            // If the current link counter reaches 0 we must go to the next depth of scanning
-            if (currLinkCounter == 0){
-                depthCounter++;
-                currLinkCounter = newLinkCounter;
-                newLinkCounter = 0;
-            }
-            
-        }
-        
-        System.out.println(elementList);
-        
-        // run crawler and then store information
-        return true;
-    }
-    */
     
     // Fu version
-    public List<TestCase> crawl2(int depth){
+    public List<TestCase> crawl(int depth){
         
         String s = String.format("Crawling for website: %s", url);
         System.out.println(s);
