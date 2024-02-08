440bc6a6802a6564777f124e91c33a7a20ba99bb
Matteo <matteomollano@gmail.com>
2024-02-08 18:49:17 -0500

    Added clone() function

--- src/backend/classes/VisitUrl.java
+++ src/backend/classes/VisitUrl.java
@@ -2,31 +2,36 @@ package backend.classes;
 
 import org.openqa.selenium.WebDriver;
 
-public class VisitUrl extends TestAction {
+public class VisitUrl extends TestAction{
     
     String url;
 
-    public VisitUrl(String url) {
+    public VisitUrl(String url){
         setURL(url);
     }
 
-    private void setURL(String url) {
+    private void setURL(String url){
         this.url = url;
     }
 
-    public String getURL() {
+    public String getURL(){
         return this.url;
     }
 
-    public void execute() {
+    public VisitUrl clone(){
+        return new VisitUrl(this.url);
+    }
+
+    public void execute(){
         WebDriver driver = MyWebDriver.getDriver();
         driver.get(this.url);
         driver.manage().window().maximize();
     }
 
     @Override
-    public String toString() {
+    public String toString(){
         String returnString = String.format("VisitUrl, url = %s", url);
         return returnString;
     }
+
 }
