988f1e995bf11d2622eaf4b4deea23c193811580
Matteo <matteomollano@gmail.com>
2024-02-08 18:53:41 -0500

    Performed crawl on dvwa web app, and called runTestCase() method on all discovered TestCases

--- src/backend/classes/Main.java
+++ src/backend/classes/Main.java
@@ -1,39 +1,52 @@
 package backend.classes;
-
-import org.openqa.selenium.WebDriver;
+import java.util.List;
+//import org.openqa.selenium.WebDriver;
 
 // For Local Tests
 public class Main {
     public static void main(String args[]) {
         System.setProperty("webdriver.chrome.driver", "chromedriver-mac-x64/chromedriver");
 
-        WebDriver driver = MyWebDriver.getDriver();
-        driver.get("http://35.175.186.100:8080/WebGoat/login");
-        System.out.println("Page title is: " + driver.getTitle());
-        System.out.println("Page URL is: " + driver.getCurrentUrl());
+        WebCrawler crawl = new WebCrawler("http://localhost/login.php", "admin", "password");
+        List<TestCase> testCases = crawl.crawl(3);
+        
+        for (int i = testCases.size()-1; i >= 0; i--) {
+            testCases.get(i).runTestCase();
+        }
+        // List<TestCase> infectedCases = attackInjector.injectCase(injectedCaseExample);
+        
+        // injectedCaseExample.display();
+        // for (TestCase tc : infectedCases) {
+            // tc.display();
+        // }
 
-        // enter text into username field
-        String usernameID = "exampleInputEmail1";
-        String usernameText = "johnny";
-        EnterText enterText = new EnterText(usernameID, usernameText);
-        enterText.execute();
+        // WebDriver driver = MyWebDriver.getDriver();
+        // driver.get("http://35.175.186.100:8080/WebGoat/login");
+        // System.out.println("Page title is: " + driver.getTitle());
+        // System.out.println("Page URL is: " + driver.getCurrentUrl());
 
-        // enter text into password field
-        String passwordID = "exampleInputPassword1";
-        String passwordText = "abc123";
-        enterText.setID(passwordID);
-        enterText.setText(passwordText);
-        enterText.execute();
+        // // enter text into username field
+        // String usernameID = "exampleInputEmail1";
+        // String usernameText = "johnny";
+        // EnterText enterText = new EnterText(usernameID, usernameText);
+        // enterText.execute();
 
-        // click the button
-        String tagName = "button";
-        ClickButton clickButton = new ClickButton(tagName);
-        clickButton.execute();
+        // // enter text into password field
+        // String passwordID = "exampleInputPassword1";
+        // String passwordText = "abc123";
+        // enterText.setID(passwordID);
+        // enterText.setText(passwordText);
+        // enterText.execute();
 
-        // check the updated URL
-        String expectedURL = driver.getCurrentUrl();
-        System.out.println("URL after login attempt: " + expectedURL);
+        // // click the button
+        // String tagName = "button";
+        // ClickButton clickButton = new ClickButton(tagName);
+        // clickButton.execute();
 
-        driver.close();
+        // // check the updated URL
+        // String expectedURL = driver.getCurrentUrl();
+        // System.out.println("URL after login attempt: " + expectedURL);
+
+        // driver.close();
     }
 }
