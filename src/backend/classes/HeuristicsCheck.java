4da2a5ad58101d25cbe952c61a120fbfac7de938
Matteo <matteomollano@gmail.com>
2024-02-08 18:52:21 -0500

    Pulled java file from origin/Justin

--- src/backend/classes/HeuristicsCheck.java
+++ src/backend/classes/HeuristicsCheck.java
@@ -3,7 +3,8 @@ package backend.classes;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.LinkedList;
-
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import java.net.MalformedURLException;
@@ -11,10 +12,10 @@ import java.net.URL;
 
 public class HeuristicsCheck {
 
-    public HeuristicsCheck() {
+    public HeuristicsCheck(){
     }
 
-    private boolean isStale(WebElement element) {
+    private boolean isStale(WebElement element){
         return ExpectedConditions.stalenessOf(element).apply(MyWebDriver.getDriver());
     }
 
@@ -24,22 +25,36 @@ public class HeuristicsCheck {
         return !parsedUrl.equals(parseURLHost(link));
     }
 
-    private boolean isNull(WebElement element) {
+    private boolean isNull(WebElement element){
         String currentLink = element.getAttribute("href");
         return currentLink == null || "".equals(currentLink);
     }
 
-    private boolean isLogout(WebElement element) {
+    private boolean isLogout(WebElement element){
+
         LinkedList<String> bannedList = new LinkedList<String>(Arrays.asList("logout", "signout", "log out", "sign out"));
         String elementText = element.getText().toLowerCase();
         return bannedList.contains(elementText);
+        
     }
 
-    private boolean isMarked(String link, HashSet<String> hashSet) {
+    private boolean isMarked(String link, HashSet<String> hashSet){
         return hashSet.contains(link);
     }
 
-    public boolean heuristicsCheck(WebElement element, String currentLink, HashSet<String> hashSet) {
+    // hardcoded for testing website
+    public boolean isBadButton(WebElement element){
+        String elementName = element.getAttribute("name");
+        if (elementName.equals("create_db")){
+            return true;
+        }
+        if (elementName.equals("Change")){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean heuristicsCheck(WebElement element, String currentLink, HashSet<String> hashSet){
         
         // if any of the heuristics hold, we can skip the web element within the crawl function
         if (isStale(element)) return true;
@@ -51,11 +66,21 @@ public class HeuristicsCheck {
         return false;
     }
 
-    public boolean canExtend(TestCase tc) {
+    public boolean canExtend(TestCase tc){
         return tc.getLast().getClass() == VisitUrl.class;
     }
+    
+    public boolean isAlertPresent() {
+        WebDriver driver = MyWebDriver.getDriver();
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
 
-    public String parseURLHost(String url) {
+    public String parseURLHost(String url){
         try {
             URL fullUrl = new URL(url);
             String startUrl = fullUrl.getHost();
@@ -65,9 +90,10 @@ public class HeuristicsCheck {
             e.printStackTrace();
         }
         return url;
+        
     }
     
-    public String parseURLPath(String url) {
+    public String parseURLPath(String url){
         try {
             URL fullUrl = new URL(url);
             String path = fullUrl.getPath();
@@ -77,4 +103,5 @@ public class HeuristicsCheck {
         }
         return url;
     }
+
 }
