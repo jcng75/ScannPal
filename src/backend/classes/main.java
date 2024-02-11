088d287429c2f9d20b3527ed83e0e4ab64f5eab7
jcng75 <justinchunng@gmail.com>
2024-02-11 13:40:43 -0500

    Updated main file to generate TestResults

--- src/backend/classes/main.java
+++ src/backend/classes/main.java
@@ -1,5 +1,6 @@
 package backend.classes;
 
+import java.util.ArrayList;
 import java.util.List;
 
 // For Local Tests
@@ -14,31 +15,13 @@ public class main {
         // System.out.println(editDistance.editDist(a, b, a.length(), a.length()));
 
         WebCrawler crawl = new WebCrawler("http://localhost/login.php", "admin", "password");
+        System.out.println(TakeScreenshot.getTimeStamp());
         List<TestCase> testCases = crawl.crawl(3);
-        
-        for (int i = testCases.size()-1; i >= 0; i--){
-            testCases.get(i).runTestCase();
+
+        List<TestResult> testResults = TestResult.generateResults(testCases);
+
+        for (TestResult tr : testResults){
+            tr.display();
         }
-        // List<TestCase> infectedCases = attackInjector.injectCase(injectedCaseExample);
-        
-        //injectedCaseExample.display();
-        // for (TestCase tc : infectedCases){
-            // tc.display();
-        // }
-        
-
-        // Testing clicking button
-        // String idName = "webwolf-button";
-        // ClickButton clickButton2 = new ClickButton(driver, idName);
-        // clickButton2.execute();
-
-        // String fileName = "screenshot.jpg";
-
-        // TakeScreenshot screenshot = new TakeScreenshot(fileName);
-        // screenshot.execute();
-
-        // List<WebElement> el = driver.findElements(By.tagName("a"));
-        // el.get(9).click();
-        
     }
 }
