f6446621fa1b34f8169e4abe20d547d09155196a
Matteo <matteomollano@gmail.com>
2024-02-16 09:45:54 -0500

    Added date and time format to filenames

--- src/backend/classes/TakeScreenshot.java
+++ src/backend/classes/TakeScreenshot.java
@@ -2,6 +2,8 @@ package backend.classes;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
@@ -9,22 +11,34 @@ import org.openqa.selenium.io.FileHandler;
 
 public class TakeScreenshot extends TestAction {
 
-   private String fileName;
+   String fileName;
    
-   public TakeScreenshot(String fileName) {
-      setFileName(fileName);
+   public TakeScreenshot(String fileName){
+      String newFileName = fileName + getTimeStamp() + ".jpg";
+      setFileName(newFileName);
       MyWebDriver.getDriver().manage().window().maximize();
    }
 
-   public void setFileName(String fileName) {
+   public TakeScreenshot(){
+      String time = getTimeStamp() + ".jpg";
+      setFileName(time);
+      MyWebDriver.getDriver().manage().window().maximize();
+   }
+
+   public void setFileName(String fileName){
       this.fileName = fileName;
    }
    
-   public String getFileName() {
+   public String getFileName(){
       return this.fileName;
    }
 
-   public void execute() {
+   public static String getTimeStamp(){
+      String fileName = new SimpleDateFormat("yyyy-MM-dd--HH-mm-ss").format(new Date());
+      return fileName;
+   }
+
+   public void execute(){
       File src = ((TakesScreenshot)MyWebDriver.getDriver()).getScreenshotAs(OutputType.FILE);
       String filePath = "photos/" + fileName;
       File newFile = new File(filePath);
