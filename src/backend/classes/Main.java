8defc4077a5b2d1d6a5ae627a4f9b8292202d00d
Matteo <matteomollano@gmail.com>
2024-02-27 16:38:19 -0500

    Main.java: Updated main to perform web crawling, TestResult generation, and scan analysis

--- src/backend/classes/Main.java
+++ src/backend/classes/Main.java
@@ -1,48 +1,23 @@
 package backend.classes;
+
+import java.io.IOException;
 import java.util.List;
-//import org.openqa.selenium.WebDriver;
 
 // For Local Tests
 public class Main {
-    public static void main(String args[]) {
+    public static void main(String args[]) throws IOException {
         System.setProperty("webdriver.chrome.driver", "chromedriver-mac-x64/chromedriver");
 
-        WebCrawler crawl = new WebCrawler("http://localhost/login.php", "admin", "password");
-        List<TestCase> testCases = crawl.crawl(3);
+        TestHttpRequest tRequest = new TestHttpRequest("http://localhost/login.php"); 
+        int responseCode = tRequest.getRequest();
+        System.out.println("Response Code: " + responseCode);
         
+        WebCrawler crawl = new WebCrawler("http://localhost/login.php", "admin", "password");
+        System.out.println(TakeScreenshot.getTimeStamp());
+        List<TestCase> testCases = crawl.crawl(3);
+
         List<TestResult> testResults = TestResult.generateResults(testCases);
 
-        for (TestResult tr: testResults) {
-            tr.display();
-        }
-
-        // WebDriver driver = MyWebDriver.getDriver();
-        // driver.get("http://35.175.186.100:8080/WebGoat/login");
-        // System.out.println("Page title is: " + driver.getTitle());
-        // System.out.println("Page URL is: " + driver.getCurrentUrl());
-
-        // // enter text into username field
-        // String usernameID = "exampleInputEmail1";
-        // String usernameText = "johnny";
-        // EnterText enterText = new EnterText(usernameID, usernameText);
-        // enterText.execute();
-
-        // // enter text into password field
-        // String passwordID = "exampleInputPassword1";
-        // String passwordText = "abc123";
-        // enterText.setID(passwordID);
-        // enterText.setText(passwordText);
-        // enterText.execute();
-
-        // // click the button
-        // String tagName = "button";
-        // ClickButton clickButton = new ClickButton(tagName);
-        // clickButton.execute();
-
-        // // check the updated URL
-        // String expectedURL = driver.getCurrentUrl();
-        // System.out.println("URL after login attempt: " + expectedURL);
-
-        // driver.close();
+        ResultAnalysis.runAnalysis(testResults);
     }
 }
