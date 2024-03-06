599dfd298d6b70868564587ae445ce688de4106b
Matteo <matteomollano@gmail.com>
2024-03-06 10:12:04 -0500

    Updated Main.java to test EC2Client

--- src/backend/classes/Main.java
+++ src/backend/classes/Main.java
@@ -1,23 +1,25 @@
 package backend.classes;
 
 import java.io.IOException;
-import java.util.List;
+// import java.util.List;
 
 // For Local Tests
 public class Main {
     public static void main(String args[]) throws IOException {
-        System.setProperty("webdriver.chrome.driver", "chromedriver-mac-x64/chromedriver");
+        // System.setProperty("webdriver.chrome.driver", "chromedriver-mac-x64/chromedriver");
 
-        TestHttpRequest tRequest = new TestHttpRequest("http://localhost/login.php"); 
-        int responseCode = tRequest.getRequest();
-        System.out.println("Response Code: " + responseCode);
+        // TestHttpRequest tRequest = new TestHttpRequest("http://localhost/login.php"); 
+        // int responseCode = tRequest.getRequest();
+        // System.out.println("Response Code: " + responseCode);
         
-        WebCrawler crawl = new WebCrawler("http://localhost/login.php", "admin", "password");
-        System.out.println(TakeScreenshot.getTimeStamp());
-        List<TestCase> testCases = crawl.crawl(3);
+        // WebCrawler crawl = new WebCrawler("http://localhost/login.php", "admin", "password");
+        // System.out.println(TakeScreenshot.getTimeStamp());
+        // List<TestCase> testCases = crawl.crawl(3);
 
-        List<TestResult> testResults = TestResult.generateResults(testCases);
+        // List<TestResult> testResults = TestResult.generateResults(testCases);
 
-        ResultAnalysis.runAnalysis(testResults);
+        // ResultAnalysis.runAnalysis(testResults);
+        EC2Client client = new EC2Client();
+        client.listInstances();
     }
 }
