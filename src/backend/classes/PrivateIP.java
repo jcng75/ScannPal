cce1b1b914926c19a2edf889bc4792567ba57d7f
jcng75 <justinchunng@gmail.com>
2024-03-10 21:56:25 -0400

    Updated PrivateIP.java implementation

--- src/backend/classes/PrivateIP.java
+++ src/backend/classes/PrivateIP.java
@@ -1,16 +1,22 @@
 
 package backend.classes;
 
-import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
  
 public class PrivateIP
 {
     public static String getPrivateIP() throws Exception
     {
-        // Returns the instance of InetAddress containing
-        InetAddress localhost = InetAddress.getLocalHost();
-        String privateIP = localhost.getHostAddress().trim();
+        String privateIP;
 
-        return privateIP;
+        Socket socket = new Socket();
+        socket.connect(new InetSocketAddress("google.com", 80));
+        privateIP = socket.getLocalAddress().toString();
+        socket.close();
+        String trimmedIP = privateIP.substring(1);
+
+        return trimmedIP;
     }
 }
