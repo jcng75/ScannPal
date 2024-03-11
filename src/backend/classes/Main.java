2e2e650239722c02d63c9ff67d2f7ff0cb3bd709
Matteo <matteomollano@gmail.com>
2024-03-10 21:10:04 -0400

    Main.java: Updated main to test MySQLConnection methods

--- src/backend/classes/Main.java
+++ src/backend/classes/Main.java
@@ -19,7 +19,23 @@ public class Main {
         // List<TestResult> testResults = TestResult.generateResults(testCases);
 
         // ResultAnalysis.runAnalysis(testResults);
-        EC2Client client = new EC2Client();
-        client.listInstances();
+        
+        // EC2Client client = new EC2Client();
+        // client.listInstances();
+
+        MySQLConnection client = new MySQLConnection();
+        // String insertQuery = """
+        //     INSERT INTO User (fname, lname, email, password_hash, creation_date) VALUES (
+        //         'Justin', 
+        //         'Ng', 
+        //         'jng14@pride.hofstra.edu', 
+        //         '$2y$10$aZegIS2LjnykNb3QPIsnM.OeQ0vIEgRjfSqAlCVlzQdyHrLvBQLVK', 
+        //         NOW()
+        //     );
+        // """;
+        // client.runUpdate(insertQuery);
+
+        String selectQuery = "SELECT * FROM User";
+        client.runSelect(selectQuery);
     }
 }
