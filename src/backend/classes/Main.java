37da62f97f53435e091026b6864fc920942f21d8
Matteo <matteomollano@gmail.com>
2024-03-15 14:07:17 -0400

    Main.java: Updated main to test functions in Serialize.java class

--- src/backend/classes/Main.java
+++ src/backend/classes/Main.java
@@ -1,11 +1,13 @@
 package backend.classes;
 
 import java.io.IOException;
-// import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 // For Local Tests
 public class Main {
-    public static void main(String args[]) throws IOException {
+    public static void main(String args[]) throws IOException, ClassNotFoundException {
         // System.setProperty("webdriver.chrome.driver", "chromedriver-mac-x64/chromedriver");
 
         // TestHttpRequest tRequest = new TestHttpRequest("http://localhost/login.php"); 
@@ -23,7 +25,7 @@ public class Main {
         // EC2Client client = new EC2Client();
         // client.listInstances();
 
-        MySQLConnection conn = new MySQLConnection();
+        // MySQLConnection conn = new MySQLConnection();
         // String createQuery = """
         //     CREATE TABLE Result (
         //         result_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -38,14 +40,40 @@ public class Main {
         // """;
         // client.runUpdate(createQuery);
 
-        String selectQuery = "SELECT * FROM User";
-        conn.displaySelectAsList(selectQuery);
-        conn.displaySelectAsTable(selectQuery);
+        // String selectQuery = "SELECT * FROM User";
+        // conn.displaySelectAsList(selectQuery);
+        // conn.displaySelectAsTable(selectQuery);
         
         // try {
 		// 	System.out.println(PrivateIP.getPrivateIP());
 		// } catch (Exception e) {
 		// 	e.printStackTrace();
 		// }
+
+        TestCase tc1 = new TestCase();
+        tc1.setAttackType("SQL tc1");
+        tc1.setInjected(false);
+        tc1.setPayload("this is for tc1 test");
+
+        TestCase tc2 = new TestCase();
+        tc2.setAttackType("XSS tc2");
+        tc2.setInjected(true);
+        tc2.setPayload("this is for tc2 test");
+
+        List<TestCase> tcList = new ArrayList<TestCase>(Arrays.asList(tc1, tc2));
+        System.out.println("tcList before serialization: " + tcList);
+
+        byte[] serializedArray = Serialize.serializeList(tcList);
+        System.out.println("Serialized tcList into byte array: " + serializedArray);
+
+        List<TestCase> deserializedTC = Serialize.deserializeList(serializedArray);
+		System.out.println("Deserialized byte array into List<TestCase>: " + deserializedTC + "\n");
+
+        for (TestCase tc : deserializedTC) {
+            System.out.println(tc.getAttackType());
+            System.out.println(tc.getInjected());
+            System.out.println(tc.getPayload()+"\n");
+        }
+ 
     }
 }
