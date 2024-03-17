7c7f55125f8c8281a343cace2ddbec431a976057
jcng75 <justinchunng@gmail.com>
2024-03-17 12:44:39 -0400

    App.java updated args

--- src/backend/classes/App.java
+++ src/backend/classes/App.java
@@ -1,16 +1,23 @@
 package backend.classes;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 // For Cloud Tests
 public class App {
     public static void main(String args[]) throws Exception {
-        System.setProperty("webdriver.chrome.driver", "/home/ec2-user/ScannPal/chromedriver-linux64/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "/ScannPal/chromedriver-linux64/chromedriver");
         ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.setBinary("/home/ec2-user/ScannPal/chrome-linux64/chrome");
+        chromeOptions.setBinary("/ScannPal/chrome-linux64/chrome");
         chromeOptions.addArguments("--headless");
-        WorkerNode workerNode = new WorkerNode();
-        workerNode.runNode();
+        chromeOptions.addArguments("--no-sandbox");
+        System.out.println("You made it here");
+        // WebDriver driver = MyWebDriver.getDriver();
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.get("http://44.212.26.250/login.php");
+        System.out.println(driver.getTitle());
+        // WorkerNode workerNode = new WorkerNode();
+        // workerNode.runNode();
     }
 }
