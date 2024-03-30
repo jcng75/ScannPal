d6b67e8bca843264743cfe814e522993362b6c7d
Matteo <matteomollano@gmail.com>
2024-03-30 10:54:27 -0400

    Serialize.java: moved serialization testing from Main.java to Serialize.java using testSerialization() method

--- src/backend/classes/Serialize.java
+++ src/backend/classes/Serialize.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Serialize {
     
@@ -33,5 +35,32 @@ public class Serialize {
             System.out.printf("IOException '%s' is caught\n", e);
         }
         return null;
+    }
+
+    public static void testSerialization() throws ClassNotFoundException {
+        TestCase tc1 = new TestCase();
+        tc1.setAttackType("SQL tc1");
+        tc1.setInjected(false);
+        tc1.setPayload("this is for tc1 test");
+
+        TestCase tc2 = new TestCase();
+        tc2.setAttackType("XSS tc2");
+        tc2.setInjected(true);
+        tc2.setPayload("this is for tc2 test");
+
+        List<TestCase> tcList = new ArrayList<TestCase>(Arrays.asList(tc1, tc2));
+        System.out.println("tcList before serialization: " + tcList);
+
+        byte[] serializedArray = Serialize.serializeList(tcList);
+        System.out.println("Serialized tcList into byte array: " + serializedArray);
+
+        List<TestCase> deserializedTC = Serialize.deserializeList(serializedArray);
+		System.out.println("Deserialized byte array into List<TestCase>: " + deserializedTC + "\n");
+
+        for (TestCase tc : deserializedTC) {
+            System.out.println(tc.getAttackType());
+            System.out.println(tc.getInjected());
+            System.out.println(tc.getPayload()+"\n");
+        }
     }
 }
