be08df9b871d22345c6fede10c5f2f2b51e03a46
jcng75 <justinchunng@gmail.com>
2024-04-09 20:42:45 -0400

    server.js updated all routes with pageTitle, added test user data for home

--- src/frontend/server.js
+++ src/frontend/server.js
@@ -14,20 +14,39 @@ app.set('views', 'views');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-  res.render('pages/index');
+  res.render('pages/index', {
+    pageTitle: 'Welcome'
+  });
 });
 
 app.get('/about', function(req, res) {
-  res.render('pages/about');
+  res.render('pages/about', {
+    pageTitle: 'About'
+  });
 });
 
 app.get('/login', function(req, res) {
-  res.render('pages/login');
+  res.render('pages/login', {
+    pageTitle: 'Login'
+  });
 });
+
+const user = {
+  first_name: 'Justin',
+  last_name: 'Ng'
+}
+
+app.get('/home', function(req, res) {
+  res.render('pages/home', {
+    pageTitle: 'Home',
+    user: user
+  });
+})
 
 app.get('/test', function(req, res) {
   res.render('pages/test', {
-    jumboTitle: "NEW EJS TITLE"
+    pageTitle: 'Test',
+    jumboTitle: 'EJS IS COOL'
   });
 });
 
