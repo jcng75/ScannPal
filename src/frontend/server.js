0010e8287c153c93a7c22215d6f2cb847aff60f5
Matteo <matteomollano@gmail.com>
2024-04-10 11:18:23 -0400

    Updated routes in server.js

--- src/frontend/server.js
+++ src/frontend/server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-import path from "path";
 
 const app = express();
 const port = 3000;
@@ -14,24 +13,45 @@ app.set('views', 'views');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-  res.render('pages/index');
+  res.render('pages/index', {
+    pageTitle: 'Welcome'
+  });
 });
 
 app.get('/about', function(req, res) {
-  res.render('pages/about');
+  res.render('pages/about', {
+    pageTitle: 'About'
+  });
 });
 
 app.get('/login', function(req, res) {
-  res.render('pages/login');
+  res.render('pages/login', {
+    pageTitle: 'Login'
+  });
 });
 
 app.get('/register', function(req, res) {
-  res.render('pages/register');
+  res.render('pages/register', {
+    pageTitle: 'Register'
+  });
+});
+
+const user = {
+  first_name: 'Justin',
+  last_name: 'Ng'
+}
+
+app.get('/home', function(req, res) {
+  res.render('pages/home', {
+    pageTitle: 'Home',
+    user: user
+  });
 });
 
 app.get('/test', function(req, res) {
   res.render('pages/test', {
-    jumboTitle: "NEW EJS TITLE"
+    pageTitle: 'Test',
+    jumboTitle: 'EJS IS COOL'
   });
 });
 
