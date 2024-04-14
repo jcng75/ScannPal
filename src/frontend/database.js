00ac6ec6fa502ba8ab23ded510cb3deb73986883
Adam Ramasre <adam.ramasre@gmail.com>
2024-04-14 18:32:38 -0400

    pulled code from Justin

---
+++ src/frontend/database.js
@@ -0,0 +1,115 @@
+import mysql from 'mysql';
+import dotenv from 'dotenv';
+import bcrypt from 'bcrypt';
+const saltRounds = 10;
+
+dotenv.config({path:'../../.env'});
+
+function createConnection() {
+    const host = process.env.MYSQL_HOST;
+    const user = process.env.MYSQL_USERNAME;
+    const password = process.env.MYSQL_PASSWORD;
+    const database = process.env.MYSQL_DB;
+    
+    try {
+        const conn = mysql.createConnection({
+            host: host,
+            user: user,
+            password: password,
+            database: database
+        });
+        return conn;
+    } catch (err) {
+        console.error(`Error creating database connection: ${err.message}`);
+        throw err;
+    }
+}
+
+function runQuery(sql) {
+    const conn = createConnection();
+
+    conn.connect(function(err) {
+        if (err) {
+            console.error(`Error connecting to MySQL database: ${err.message}`);
+            throw err;
+        } 
+        
+        console.log("Connected to MySQL database!");
+        conn.query(sql, function(err, results) {
+            if (err) {
+                console.error(`Error executing query '${sql}': ${err.message}`);
+                throw err;
+            }
+            
+            // display results here ...
+            console.log(`Running ${sql}\n`);
+            
+            // get column names
+            const columns = Object.keys(results[0]);
+
+            results.forEach((row) => {
+                columns.forEach((col) => {
+                    console.log(`${col}: ${row[col]}`);
+                });
+                console.log();
+            });
+
+            conn.end();
+        });
+    
+    });
+
+}
+
+// how to call function:
+// const password_hash = await hash("goodyear");
+async function hash(password) {
+    try {
+        const salt = await bcrypt.genSalt(saltRounds);
+        console.log(`Salt: ${salt}`);
+
+        const hash = await bcrypt.hash(password, salt);
+        console.log(`Hash: ${hash}`);
+
+        return hash;
+    } catch (err) {
+        console.error(`Error hashing password: ${err.message}`);
+    }
+}
+
+function createUser(firstName, lastName, email, password) {
+    const conn = createConnection();
+
+    conn.connect(async function(err) {
+        if (err) {
+            console.error(`Error connecting to MySQL database: ${err.message}`);
+            throw err;
+        } 
+        
+        console.log("Connected to MySQL database!");
+
+        // perform password hashing here...
+        const password_hash = await hash(password);
+        
+        // format the sql query
+        let sql = `INSERT INTO User (fname, lname, email, password_hash, creation_date) VALUES (?, ?, ?, ?, NOW());`;
+        const inserts = [firstName, lastName, email, password_hash];
+        sql = mysql.format(sql, inserts);
+
+        console.log(sql);
+        
+        // run the query
+        conn.query(sql, function(err, results) {
+            if (err) {
+                console.error(`Error executing query '${sql}': ${err.message}`);
+                throw err;
+            }
+            console.log("Successfully added new user to the database!");
+            console.log(JSON.stringify(results, null, 2));
+        });
+      
+    });
+}
+
+// createUser('Adam', 'Ramasre', 'aramasre1@pride.hofstra.edu', 'goodyear');
+// runQuery("SELECT * FROM User");
---
+++ src/frontend/server.js
@@ -0,0 +1,82 @@
+import express from "express";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import dotenv from 'dotenv';
+
+const app = express();
+dotenv.config({path:'../../.env'});
+const port = process.env.PORT;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(express.static(__dirname + '/public'));
+
+// API Middlewares
+app.use(express.json()); // to accept data in JSON format
+app.use(express.urlencoded({ extended: true })); // to decode data that is sent through an html form
+
+app.get('/', function(req, res) {
+  res.render('pages/index', {
+    pageTitle: 'Welcome'
+  });
+});
+
+app.get('/about', function(req, res) {
+  res.render('pages/about', {
+    pageTitle: 'About'
+  });
+});
+
+app.get('/login', function(req, res) {
+  res.render('pages/login', {
+    pageTitle: 'Login'
+  });
+});
+
+app.post('/login', function(req, res) {
+  res.send(req.body);
+});
+
+app.get('/register', function(req, res) {
+  res.render('pages/register', {
+    pageTitle: 'Register'
+  });
+});
+
+app.post('/register', function(req, res) {
+  res.send(req.body);
+});
+
+app.get('/logout', function(req, res) {
+  res.render('pages/logout', {
+    pageTitle: 'Logout',
+  });
+});
+
+const user = {
+  first_name: 'Justin',
+  last_name: 'Ng',
+  last_scan: '4/12/2024',
+  total_scans: 3
+}
+
+app.get('/home', function(req, res) {
+  res.render('pages/home', {
+    pageTitle: 'Home',
+    user: user
+  });
+});
+
+app.get('/test', function(req, res) {
+  res.render('pages/test', {
+    pageTitle: 'Test',
+    jumboTitle: 'EJS IS COOL'
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}!`);
+});
