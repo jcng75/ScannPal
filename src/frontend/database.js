acc4d608e2737e640239f1fdcb87e2767490a5ec
Matteo <matteomollano@gmail.com>
2024-04-16 19:36:37 -0400

    database.js: Added emailExists(), validateHash(), and getUser() functions

--- src/frontend/database.js
+++ src/frontend/database.js
@@ -1,10 +1,10 @@
 import mysql from 'mysql';
 import dotenv from 'dotenv';
 import bcrypt from 'bcrypt';
-const saltRounds = 10;
 
 dotenv.config({path:'../../.env'});
 
+// create a connection to the MySQL database
 function createConnection() {
     const host = process.env.MYSQL_HOST;
     const user = process.env.MYSQL_USERNAME;
@@ -25,7 +25,8 @@ function createConnection() {
     }
 }
 
-function runQuery(sql) {
+// print the results of a SQL query
+function displayQuery(sql) {
     const conn = createConnection();
 
     conn.connect(function(err) {
@@ -53,23 +54,44 @@ function runQuery(sql) {
                 });
                 console.log();
             });
-
-            conn.end();
         });
-    
-    });
 
+        conn.end();
+    });
 }
 
+// return the results of a SQL query as an object
+function getQueryResults(sql) {
+    const conn = createConnection();
+    
+    conn.connect(function(err) {
+        if (err) {
+            console.error(`Error connecting to MySQL database: ${err.message}`);
+            throw err;
+        } 
+        conn.query(sql, function(err, results) {
+            if (err) {
+                console.error(`Error executing query '${sql}': ${err.message}`);
+                throw err;
+            }
+            return results;
+        });
+
+        conn.end();
+    });
+}
+
+// generates a bcrypt password hash for a user-entered password
 // how to call function:
 // const password_hash = await hash("goodyear");
-async function hash(password) {
+export async function hash(password) {
+    const saltRounds = 10;
     try {
         const salt = await bcrypt.genSalt(saltRounds);
-        console.log(`Salt: ${salt}`);
+        // console.log(`Salt: ${salt}`);
 
         const hash = await bcrypt.hash(password, salt);
-        console.log(`Hash: ${hash}`);
+        // console.log(`Hash: ${hash}`);
 
         return hash;
     } catch (err) {
@@ -77,7 +99,8 @@ async function hash(password) {
     }
 }
 
-function createUser(firstName, lastName, email, password) {
+// add a new user to the User table
+export function createUser(firstName, lastName, email, password) {
     const conn = createConnection();
 
     conn.connect(async function(err) {
@@ -107,9 +130,107 @@ function createUser(firstName, lastName, email, password) {
             console.log("Successfully added new user to the database!");
             console.log(JSON.stringify(results, null, 2));
         });
-      
+
+        conn.end();
     });
 }
 
-// createUser('Adam', 'Ramasre', 'aramasre1@pride.hofstra.edu', 'goodyear');
-// runQuery("SELECT * FROM User");
+// checks if an email exists in the database
+export async function emailExists(email) {
+    const conn = createConnection();
+
+    return new Promise((resolve, reject) => {
+        conn.connect(function(err) {
+            if (err) {
+                console.error(`Error connecting to MySQL database: ${err.message}`);
+                reject(err);
+                return;
+            }
+
+            let sql = `SELECT * FROM User where email = ?;`;
+            const inserts = [email];
+            sql = mysql.format(sql, inserts);
+
+            conn.query(sql, function(err, res) {
+                if (err) {
+                    console.error(`Error executing query '${sql}': ${err.message}`);
+                    reject(err);
+                    return;
+                }
+                resolve(res.length > 0);
+            });
+
+            conn.end();
+        });
+    });
+}
+
+// returns true if the user-entered password matches the hash stored in the database
+// returns false otherwise
+export async function validateHash(email, password) {
+    const conn = createConnection();
+
+    return new Promise((resolve, reject) => {
+        conn.connect(function(err) {
+            if (err) {
+                console.error(`Error connecting to MySQL database: ${err.message}`);
+                reject(err);
+                return;
+            }
+
+            let sql = `SELECT password_hash FROM User where email = ?;`;
+            const inserts = [email];
+            sql = mysql.format(sql, inserts);
+            
+            conn.query(sql, async function(err, results) {
+                if (err) {
+                    console.error(`Error executing query '${sql}': ${err.message}`);
+                    reject(err);
+                    return;
+                }
+
+                // perform hash validation here ...
+                const hash = String(results[0].password_hash);
+                try {
+                    const isValidHash = await bcrypt.compare(password, hash);
+                    resolve(isValidHash);
+                } catch (err) {
+                    reject(err);
+                }
+            });
+            
+            conn.end();
+        });
+    });
+}
+
+// returns user information as an object
+export async function getUser(email) {
+    const conn = createConnection();
+
+    return new Promise((resolve, reject) => {
+        conn.connect(function(err) {
+            if (err) {
+                console.error(`Error connecting to MySQL database: ${err.message}`);
+                reject(err);
+                return;
+            } 
+
+            // format the sql query
+            let sql = `SELECT fname, lname FROM User where email = ?;`;
+            const inserts = [email];
+            sql = mysql.format(sql, inserts);
+
+            conn.query(sql, function(err, results) {
+                if (err) {
+                    console.error(`Error executing query '${sql}': ${err.message}`);
+                    reject(err);
+                    return;
+                }
+                resolve(results[0]);
+            });
+
+            conn.end();
+        });
+    });
+}
