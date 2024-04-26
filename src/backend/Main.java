7e71b6128b828cd162130510e84061d1b0f5df5e
jcng75 <justinchunng@gmail.com>
2024-04-26 13:52:24 -0400

    Main.java now includes updated url for creating tasks

--- src/backend/Main.java
+++ src/backend/Main.java
@@ -20,13 +20,16 @@ import java.util.HashMap;
 public class Main {
     public static void main(String args[]) throws IOException, SQLException, ClassNotFoundException {
         System.setProperty("webdriver.chrome.driver", "/ScannPal/chromedriver-linux64/chromedriver");
+
         // url username password userID passID depth email
         WebCrawler crawl = new WebCrawler(args[0], args[1], args[2], args[3], args[4]);
         int depth = Integer.parseInt(args[5]);
+        String parsedUrl = crawl.getParsedUrl();
         List<TestCase> testCases = crawl.crawl(depth);
         List<List<TestCase>> injectedCases = AttackInjector.generateInjectedCases(testCases);
         HashMap<String, List<List<TestCase>>> splitCases = AttackInjector.splitTestCases(injectedCases);
-        WorkerNode.createTasks(splitCases, args[6]);
-        MyWebDriver.getDriver().close();
+        WorkerNode.createTasks(splitCases, args[6], parsedUrl);
+        MyWebDriver.getDriver().quit();
+        System.out.println("(+) Scan has been completed!");
     }
 }
