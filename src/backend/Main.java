7f8f905a85483011881c280077b5885afc21518d
Matteo <matteomollano@gmail.com>
2024-04-26 21:04:10 -0400

    Updated Main.java for EC2 webserver

--- src/backend/Main.java
+++ src/backend/Main.java
@@ -1,58 +1,35 @@
 package backend;
 
 import java.io.IOException;
-// import java.sql.Connection;
-// import java.sql.PreparedStatement;
-// import java.sql.ResultSet;
-// import java.sql.SQLException;
-// import java.util.ArrayList;
-// import java.util.Arrays;
-// import java.util.List;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 import backend.aws.MySQLConnection;
+import backend.results.TestCase;
+import backend.scan.AttackInjector;
+import backend.scan.MyWebDriver;
+import backend.scan.WebCrawler;
+import backend.scan.WorkerNode;
+
+import java.util.HashMap;
 
 // For Local Tests
 public class Main {
-    public static void main(String args[]) throws IOException, ClassNotFoundException {
-        System.setProperty("webdriver.chrome.driver", "chromedriver-mac-x64/chromedriver");
+    public static void main(String args[]) throws IOException, SQLException, ClassNotFoundException {
+        System.setProperty("webdriver.chrome.driver", "/ScannPal/chromedriver-linux64/chromedriver");
 
-        // TestHttpRequest tRequest = new TestHttpRequest("http://localhost/login.php"); 
-        // int responseCode = tRequest.getRequest();
-        // System.out.println("Response Code: " + responseCode);
-        
-        // WebCrawler crawl = new WebCrawler("http://localhost/login.php", "admin", "password");
-        // System.out.println(TakeScreenshot.getTimeStamp());
-        // List<TestCase> testCases = crawl.crawl(3);
-
-        // List<TestResult> testResults = TestResult.generateResults(testCases);
-
-        // ResultAnalysis.runAnalysis(testResults);
-        
-        // EC2Client client = new EC2Client();
-        // client.listInstances();
-
-        MySQLConnection conn = new MySQLConnection();
-
-        // conn.runUpdate("DELETE FROM Result");
-        // conn.runUpdate("DELETE FROM Task");
-        // conn.runUpdate("DELETE FROM Job");
-
-        String selectQuery;
-
-        // selectQuery = "SELECT * FROM User";
-        // conn.displaySelectAsTable(selectQuery);
-
-        selectQuery = "SELECT * FROM Job";
-        conn.displaySelectAsTable(selectQuery);
-
-        // selectQuery = "SELECT * FROM Task";
-        // conn.displaySelectAsTable(selectQuery);
-
-        // selectQuery = "SELECT * FROM Result";
-        // conn.displaySelectAsList(selectQuery);
-
-        // conn.runUpdate("ALTER TABLE Job ADD COLUMN website_link VARCHAR(255) AFTER user_id;");
-        // conn.runUpdate("UPDATE Job SET website_link = 'www.example.com' WHERE job_id = 19;");
-
+        // url username password userID passID depth email
+        WebCrawler crawl = new WebCrawler(args[0], args[1], args[2], args[3], args[4]);
+        int depth = Integer.parseInt(args[5]);
+        String parsedUrl = crawl.getParsedUrl();
+        List<TestCase> testCases = crawl.crawl(depth);
+        List<List<TestCase>> injectedCases = AttackInjector.generateInjectedCases(testCases);
+        HashMap<String, List<List<TestCase>>> splitCases = AttackInjector.splitTestCases(injectedCases);
+        WorkerNode.createTasks(splitCases, args[6], parsedUrl);
+        MyWebDriver.getDriver().quit();
+        System.out.println("(+) Scan has been completed!");
     }
 }
